17576d777ecd898c95cf7d4c9282ee66db6fa959
Anthony Olu <b.ioanth1000@gmail.com>
2016-04-27 11:49:52 +0100

    Initial Commit

---
+++ Tip Calculator App/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator App
+//
+//  Created by MBPinTheAir on 27/04/2016.
+//  Copyright © 2016 moorsideinc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
